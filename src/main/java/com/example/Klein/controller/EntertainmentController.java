331d04414156d80ab148a6ef607dc0f328272ebe
knight <2899626547@qq.com>
2022-07-10 15:52:41 +0800

    娱乐和Romm类增删查改

--- src/main/java/com/example/Klein/controller/EntertainmentController.java
+++ src/main/java/com/example/Klein/controller/EntertainmentController.java
@@ -1,7 +1,10 @@
 package com.example.Klein.controller;
 
 import com.example.Klein.entity.Entertainment;
+import com.example.Klein.entity.ScenicArea;
 import com.example.Klein.service.EntertainmentService;
+import com.example.Klein.service.ScenicAreaService;
+import com.example.Klein.utils.result.Result;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,15 +25,18 @@ public class EntertainmentController {
     @Resource
     private EntertainmentService entertainmentService;
 
+    @Resource
+    private ScenicAreaService scenicAreaService;
+
     /**
      * 通过主键查询单条数据
      *
-     * @param id 主键
+     * @param EntertainmentId 主键
      * @return 单条数据
      */
-    @GetMapping("{id}")
-    public ResponseEntity<Entertainment> queryById(@PathVariable("id") Long id) {
-        return ResponseEntity.ok(this.entertainmentService.queryById(id));
+    @PostMapping("/queryById")
+    public Result queryById(@RequestBody long EntertainmentId){
+        return Result.success(this.entertainmentService.queryById(EntertainmentId));
     }
 
     /**
@@ -39,9 +45,27 @@ public class EntertainmentController {
      * @param entertainment 实体
      * @return 新增结果
      */
-    @PostMapping
-    public ResponseEntity<Entertainment> add(Entertainment entertainment) {
-        return ResponseEntity.ok(this.entertainmentService.insert(entertainment));
+    @PostMapping("/EntertainmentAdd")
+    public Result EntertainmentAdd(@RequestBody Entertainment entertainment) {
+        if(entertainment.getEntertainmentId()!=null){
+            return Result.fail(400,"主键不允许自定义增加",entertainment);
+        }
+        if(entertainment.getScenicAreaId()==null){
+            return Result.fail(400,"增加对象时外键不允许为空",entertainment);
+        }
+        if(entertainment.getScenicAreaId()!=null){
+            ScenicArea scenicArea=scenicAreaService.queryById(entertainment.getScenicAreaId());
+            if (scenicArea==null){
+                return Result.fail(400,"外键scenicArea在表中不存在",entertainment);
+            }
+        }
+        //正式添加
+        Entertainment _entertainment = this.entertainmentService.insert(entertainment);
+        try {
+            return Result.success(200, "添加成功", _entertainment);
+        } catch (Exception e) {
+            return Result.fail(402, "添加失败", _entertainment.getEntertainmentId());
+        }
     }
 
     /**
@@ -50,20 +74,43 @@ public class EntertainmentController {
      * @param entertainment 实体
      * @return 编辑结果
      */
-    @PutMapping
-    public ResponseEntity<Entertainment> edit(Entertainment entertainment) {
-        return ResponseEntity.ok(this.entertainmentService.update(entertainment));
+    @PostMapping("/updateEntertainment")
+            public Result updateEntertainment(@RequestBody Entertainment entertainment) {
+                if(entertainment.getEntertainmentId()==null){
+                    return Result.fail(400,"必须经过主键进行更新但主键为空",null);
+                }
+                Entertainment Testentertainment=this.entertainmentService.queryById(entertainment.getEntertainmentId());
+                if(Testentertainment==null){
+                    return Result.fail(400,"表中无此主键ID对应的数据",null);
+                }
+                if (entertainment.getScenicAreaId() != null) {
+                    return Result.fail(400, "不允许修改外键ScenicAreaId,此项必须为空", null);
+                }
+                Entertainment _entertainment = this.entertainmentService.update(entertainment);
+                if (_entertainment != null) {
+                    return Result.success(200, "更新成功", _entertainment);
+                } else {
+                    return Result.fail(400, "更新失败", null);
+        }
     }
-
     /**
      * 删除数据
      *
-     * @param id 主键
+     * @param entertainmentId 主键
      * @return 删除是否成功
      */
-    @DeleteMapping
-    public ResponseEntity<Boolean> deleteById(Long id) {
-        return ResponseEntity.ok(this.entertainmentService.deleteById(id));
+    @DeleteMapping("/deleteEntertainmentById")
+    public Result deleteEntertainmentById(@RequestBody Long entertainmentId) {
+        Entertainment _entertainment=this.entertainmentService.queryById(entertainmentId);
+        if(_entertainment==null){
+            return Result.fail(400,"数据库不存在ID对应的列",null);
+        }
+        boolean mark = this.entertainmentService.deleteById(entertainmentId);
+        if(mark){
+            return Result.success(200,"删除成功",null);
+        }else{
+            return Result.fail(400,"删除失败",null);
+        }
     }
 
 }
--- src/main/java/com/example/Klein/controller/GuestRoomController.java
+++ src/main/java/com/example/Klein/controller/GuestRoomController.java
@@ -1,7 +1,10 @@
 package com.example.Klein.controller;
 
+import com.example.Klein.entity.Entertainment;
 import com.example.Klein.entity.GuestRoom;
+import com.example.Klein.entity.ScenicArea;
 import com.example.Klein.service.GuestRoomService;
+import com.example.Klein.utils.result.Result;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,45 +28,85 @@ public class GuestRoomController {
     /**
      * 通过主键查询单条数据
      *
-     * @param id 主键
+     * @param GuestRoomId 主键
      * @return 单条数据
      */
-    @GetMapping("{id}")
-    public ResponseEntity<GuestRoom> queryById(@PathVariable("id") Long id) {
-        return ResponseEntity.ok(this.guestRoomService.queryById(id));
+    @PostMapping("/queryById")
+    public Result queryById(@RequestBody long GuestRoomId){
+        return Result.success(this.guestRoomService.queryById(GuestRoomId));
     }
 
-    /**
-     * 新增数据
-     *
-     * @param guestRoom 实体
-     * @return 新增结果
+    /**新增数据
+     *@param guestRoom 实体
+     *@return 新增结果
      */
-    @PostMapping
-    public ResponseEntity<GuestRoom> add(GuestRoom guestRoom) {
-        return ResponseEntity.ok(this.guestRoomService.insert(guestRoom));
+    @PostMapping("/GuestRoomAdd")
+    public Result GuestRoomAdd(@RequestBody GuestRoom guestRoom) {
+        if(guestRoom.getRoomId()!=null){
+            return Result.fail(400,"主键不允许自定义增加",guestRoom);
+        }
+        if(guestRoom.getHotelId()==null){
+            return Result.fail(400,"增加对象时外键不允许为空",guestRoom);
+        }
+        if(guestRoom.getRoomId()!=null){
+            GuestRoom newGuestRoom=guestRoomService.queryById(guestRoom.getRoomId());
+            if (newGuestRoom==null){
+                return Result.fail(400,"外键scenicArea在表中不存在",guestRoom);
+            }
+        }
+        //正式添加
+        GuestRoom _guestRoom = this.guestRoomService.insert(guestRoom);
+        try {
+            return Result.success(200, "添加成功", _guestRoom);
+        } catch (Exception e) {
+            return Result.fail(402, "添加失败", _guestRoom);
+        }
     }
+
 
     /**
      * 编辑数据
-     *
      * @param guestRoom 实体
      * @return 编辑结果
      */
-    @PutMapping
-    public ResponseEntity<GuestRoom> edit(GuestRoom guestRoom) {
-        return ResponseEntity.ok(this.guestRoomService.update(guestRoom));
+    @PostMapping("/updateGuestRoom")
+    public Result updateGuestRoom(@RequestBody GuestRoom guestRoom) {
+        if(guestRoom.getRoomId()==null){
+            return Result.fail(400,"必须经过主键进行更新但主键为空",null);
+        }
+        GuestRoom TestGuestRoom=this.guestRoomService.queryById(guestRoom.getRoomId());
+        if(TestGuestRoom==null){
+            return Result.fail(400,"表中无此主键ID对应的数据",null);
+        }
+        if (guestRoom.getHotelId()!= null) {
+            return Result.fail(400, "不允许修改外键ScenicAreaId,此项必须为空", null);
+        }
+        GuestRoom _guestRoom = this.guestRoomService.update(guestRoom);
+        if (_guestRoom != null) {
+            return Result.success(200, "更新成功", _guestRoom);
+        } else {
+            return Result.fail(400, "更新失败", null);
+        }
     }
 
     /**
      * 删除数据
      *
-     * @param id 主键
+     * @param guestRoomId 主键
      * @return 删除是否成功
      */
-    @DeleteMapping
-    public ResponseEntity<Boolean> deleteById(Long id) {
-        return ResponseEntity.ok(this.guestRoomService.deleteById(id));
+    @DeleteMapping("/deleteGuestRoomById")
+    public Result deleteGuestRoomById(@RequestBody Long guestRoomId) {
+        GuestRoom _guestRoom=this.guestRoomService.queryById(guestRoomId);
+        if(_guestRoom==null){
+            return Result.fail(400,"数据库不存在ID对应的列",null);
+        }
+        boolean mark = this.guestRoomService.deleteById(guestRoomId);
+        if(mark){
+            return Result.success(200,"删除成功",null);
+        }else{
+            return Result.fail(400,"删除失败",null);
+        }
     }
 
 }
